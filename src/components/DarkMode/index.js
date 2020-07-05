6c9ee36be4df6d69bacc3920b89f14b85fa9a8b8
Arunkumar Palaniappan <11667975+arunkumarpalaniappan@users.noreply.github.com>
2020-07-05 08:25:21 +0000

    Title Change

--- src/components/DarkMode/index.js
+++ src/components/DarkMode/index.js
@@ -5,7 +5,7 @@ import ToggleButton from 'react-toggle-button'
 const Check = () => (
     <svg width="14" height="10" viewBox="0 0 14 11" xmlns="http://www.w3.org/2000/svg">
       <title>
-        switch-check
+      Switch Color Theme
       </title>
       <path
         d="M11.264 0L5.26 6.004 2.103 2.847 0 4.95l5.26 5.26 8.108-8.107L11.264 0"
@@ -17,7 +17,7 @@ const Check = () => (
 const X = () => (
     <svg width="10" height="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
       <title>
-        switch-x
+        Switch Color Theme
       </title>
       <path
         d="M9.9 2.12L7.78 0 4.95 2.828 2.12 0 0 2.12l2.83 2.83L0 7.776 2.123 9.9 4.95 7.07 7.78 9.9 9.9 7.776 7.072 4.95 9.9 2.12"
@@ -28,7 +28,7 @@ const X = () => (
 
 const DarkModeToggle = () => {
   const darkMode = useDarkMode(false);
-    const [value, setValue] = useState(darkMode.value);
+  const [value, setValue] = useState(darkMode.value);
   return (
     <div className={'theme-toggle-button'}>
       Dark Theme :  <ToggleButton
