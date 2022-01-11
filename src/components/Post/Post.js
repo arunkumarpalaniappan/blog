916724b66b4bbde81d48860de9207b3953cbd85d
Arunkumar Palaniappan <11667975+arunkumarpalaniappan@users.noreply.github.com>
2022-01-11 09:56:26 +0530

    Update Post.js

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby';
 import Author from './Author';
 import Comments from './Comments';
 import Content from './Content';
-import DarkModeToggle from '../DarkMode';
+// import DarkModeToggle from '../DarkMode';
 import Meta from './Meta';
 import Tags from './Tags';
 import styles from './Post.module.scss';
@@ -26,7 +26,7 @@ const Post = ({ post }: Props) => {
     <div className={styles['post']}>
       <Link id="white-logo" className={styles['post__home-button']} to="/"><span style={{ fontFamily: 'Sacramento', fontSize: 45 }}>Arunkumar<br/>Palaniappan</span></Link>
       <Link id="dark-logo" className={styles['post__home-button']} to="/"><span style={{ fontFamily: 'Sacramento', fontSize: 45 }}>Arunkumar<br/>Palaniappan</span></Link>
-      <br/><DarkModeToggle style={{ position: 'fixed',margin: 0,top: 130,left: 90}} /><br/>
+//       <br/><DarkModeToggle style={{ position: 'fixed',margin: 0,top: 130,left: 90}} /><br/>
         <div className={styles['post__content']}>
         <Content body={html} title={title} readingTime={readingTime} date={date} />
       </div>
